610ce60ff34bb166627adc228b4d48b0c6727ab8
yerken2905 <35755850+yerken2905@users.noreply.github.com>
2018-08-29 17:48:26 +0600

    Update live.py

--- live.py
+++ live.py
@@ -1,35 +1,21 @@
 import codecs
-fileObj = codecs.open( "live.html", "r", "utf_8_sig" )
+fileObj = codecs.open( "live.htm", "r", "utf_8_sig" )
 text = fileObj.read() # или читайте по строке
 fileObj.close()
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(text, 'html.parser')
-#print(soup)
-
-import lxml.html
-from lxml import etree
-#tree=etree.HTML(text)
-#r = tree.xpath('.//div[@class=\"lt-sport-title lt-showmore\"]')
-#print(r.div)
-#html=soup.xpath('.//div[@class=\"lt-sport-title lt-showmore\"]')
+#import lxml.html
+#from lxml import etree
 
 from lxml.html.soupparser import fromstring
 tree = fromstring(text)
-#ttt=etree.tostring(tree)
-#zzz=etree.parse(ttt)
-#ch=tree.xpath('.//div[@class=\"lt-sport-title lt-showmore\"]')
-for ch in tree.xpath('.//div[@class=\"lt-sport-title lt-showmore\"]'):
-    print(ch.text)
-
-"""
-import lxml.html
-from lxml import etree
-parse=etree.parse(text)
-#xp1 = etree.XPath('.//div[@class=\"lt-sport-title lt-showmore\"]')
-#html = lxml.html.fromstring(''.join(text))
-#print(''.join(text))
-#html=html.xpath('.//div[@class=\"lt-sport-title lt-showmore\"]')
-#print(html)
-#print(lxml.html.tostring(html))
-"""
+for ch in tree.xpath('.//td[@class=\"liveMainSport forLiveFilter\"]'):
+    for ch1 in ch.xpath('.//a/b'):
+        print(ch1.text)
+    for ch2 in ch.xpath('..[@data-sport]'):
+        print(ch2.text)
+        
+#    print(ch.xpath('[@data-sport]'))
+#nsports=1
+#for nSports in range(1,10,1):
+#    ch=tree.xpath('.//td[@data-sport=\"'+str(nSports)+'\"]/a/b')
+#    print(ch[0].text)
