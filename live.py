204b22dd50f910f899af3933ebfee50611729fb9
yerken2905 <35755850+yerken2905@users.noreply.github.com>
2018-09-06 00:21:25 +0600

    Update live.py

--- live.py
+++ live.py
@@ -11,27 +11,17 @@ socket.setdefaulttimeout(timeout)
 
 aHref=[]
 
-def connect_oracle():
-    try:
-        global my_connection
-        global ocursor
-        my_connection=cx_Oracle.connect('colvir/main082018@cbsmain')
-        ocursor=my_connection.cursor()
-        print('Logon success')
-    except cx_Oracle.DatabaseError as info:
-        print('Logon  Error:',info)
-
 def mod_table(idRow, nameCol,  value):
     text='select * from z_025_temp_ol where id='+str(idRow)
     ocursor.execute(text)
-    print(text)
+    #print(idRow, nameCol,  value)
     if ocursor.fetchone():
-        text="update z_025_temp_ol set " + nameCol + "='" + value + "' where id=" + str(idRow)
+        text='update z_025_temp_ol set ' + nameCol + '=:s where id=' + str(idRow)
         #print(text)
     else:
-        text="insert into z_025_temp_ol (id,"+nameCol+") values("+ str(idRow) + ",'"+ value +"')"
+        text='insert into z_025_temp_ol (id,'+nameCol+') values('+ str(idRow) + ',:s)'
         #print(text)
-    ocursor.execute(text)
+    ocursor.execute(text,s=value)
     my_connection.commit()
 
 def first_page():
@@ -40,7 +30,6 @@ def first_page():
     req=Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     web_byte = urlopen(req).read()
     tree = fromstring(web_byte)
-    sHref=[]
     #Список ставок
     for lTable in tree.xpath('//div[@class=\"lt-match\"]'):
         DataId=lTable.get('data-id')
@@ -54,9 +43,10 @@ def first_page():
                 cText+=cRow+';'
         mod_table(DataId, 's1', cText)
 
-print(aHref)
+#print(aHref)
 
 def second_page():
+    aHref=['42827542']
     for ch in aHref:
         url='https://olimp.kz/index.php?page=line&action=2&live[]='+ch+'&sid[]=1'
         print(url)
@@ -68,15 +58,20 @@ def second_page():
         for lTable in tree.xpath('.//span[@class=\"googleStatIssue\"]'):
             for lKoefs in lTable.xpath('.//text()'):
                 cRow=lKoefs.replace('\n','').replace('\xa0','').replace('-','')
-                if len(cText)>1:
-                    #print(cRow)
+                if len(cRow)>1:
                     cText+=cRow+';'
+        #print(cText)
         mod_table(ch, 's2', cText)
 
+try:
+    my_connection=cx_Oracle.connect('yerken/1@xe')
+    ocursor=my_connection.cursor()
+    print('Logon success')
+except cx_Oracle.DatabaseError as info:
+    print('Logon  Error:',info)
 
-connect_oracle()
-print(time.ctime())
-time.sleep(100)
-first_page()
-print(time.ctime())
+#print(time.ctime())
+#time.sleep(100)
+#first_page()
+#print(time.ctime())
 second_page()
